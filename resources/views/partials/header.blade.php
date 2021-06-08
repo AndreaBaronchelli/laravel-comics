9a90342dd364d69b7d3f4e77fd9dfdcf97d8026e
Andrea Baronchelli <baronchelli.andrea@gmail.com>
2021-06-08 18:42:43 +0200

    add dynamic class to navbar

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -5,8 +5,14 @@
         <a href="{{ route('home') }}"><img src="{{ asset('images/dc-logo.png') }}" alt=""></a>
         <nav>
             <ul>
-                <li><a href="{{ route('home')}}" class="active">Comics</a></li>
-                <li><a href="{{ route('news')}}">News</a></li>
+                <li><a href="{{ route('home')}}" class="
+                    @if (url()->full() == 'http://127.0.0.1:8000')
+                        {{ 'active' }}
+                    @endif">Comics</a></li>
+                <li><a href="{{ route('news')}}" class="
+                    @if (url()->full() == 'http://127.0.0.1:8000/news')
+                        {{ 'active' }}
+                    @endif">News</a></li>
             </ul>
         </nav>
     </div>
